c3bf5b9dc45c1a42b712653e539f9b360437a799
CamiloCedenoC <154028859+CamiloCedenoC@users.noreply.github.com>
2024-02-01 01:40:20 -0500

    Delete src directory

--- src/main/java/com/unir/ms/reserva/unirmsreserva/data/ReservationJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unir.ms.reserva.unirmsreserva.data;
-
-import com.unir.ms.reserva.unirmsreserva.model.db.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationJpaRepository extends JpaRepository<Reservation, Long> {
-}
